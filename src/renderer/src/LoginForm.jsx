d93018b1d23a9d0f16b2b9d0a4bf7324d51e4050
sseezov <sseezov@mail.ru>
2025-09-10 01:17:40 +0700

    add redirect

---
+++ src/renderer/src/LoginForm.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router";
+
+function LoginForm() {
+  const navigate = useNavigate();
+
+  async function submitHandler(e) {
+    e.preventDefault()
+    const user = {
+      login: e.target.login.value,
+      password: e.target.password.value,
+    }
+    const role = await window.api.autorizeUser(user);
+    if (role === 'Администратор') {
+      navigate('/main');
+    }
+    document.querySelector('form').reset()
+  }
+
+  return (
+    <>
+      {/* <img alt="logo" className="logo" src={electronLogo} /> */}
+      <h1>Приветствие!</h1>
+      <h4>Введите логин и пароль, чтобы войти</h4>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <label htmlFor="login">Логин:</label>
+        <input id="login" type="text" required />
+        <label htmlFor="password">Пароль:</label>
+        <input id="password" type="text" required />
+        <button type="submit">Войти</button>
+      </form>
+      <h5>Перейти на экран просмотра товаров в виде гостя</h5>
+      <button>Посмотреть товары</button>
+    </>
+  )
+}
+
+export default LoginForm
+
--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -5,12 +5,14 @@ import { Routes, Route, HashRouter } from 'react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import LoginForm from './LoginForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <StrictMode>
       <Routes>
-        <Route path='/' element={<App/>}/>
+        <Route path='/' element={<LoginForm/>}/>
+        <Route path='/main' element={<App/>}/>
       </Routes>
     </StrictMode>
   </HashRouter>
